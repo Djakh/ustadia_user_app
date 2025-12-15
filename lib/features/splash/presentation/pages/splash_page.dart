bc1991fdf66e3330bc14aa9e9e54647d8edf8789
Djakh <basitxanov@gmail.com>
2025-12-15 11:21:38 +0500

    second commit

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -28,7 +28,7 @@ class _SplashPageState extends State<SplashPage> {
 
   void start() => Future.delayed(const Duration(seconds: 2), () {
         if (!mounted) return;
-        //  goToOnboarding();
+          goToOnboarding();
       });
 
   /// --- Widgets ---
